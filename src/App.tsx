4b44f9d7d6559f28475b2f3872bfc923e3b096b3
toto-alt <130222034+toto-alt@users.noreply.github.com>
2023-04-08 16:33:55 +0200

    feat: fetch my songs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
+
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const goToNextTrack = () =>
+    setTrackIndex(prevState => (prevState + 1) % trackUrls.length);
+
+  const { data: tracks } = useQuery({ queryKey: [''], queryFn: fetchTracks });
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +25,12 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur mon blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>You currently have {tracks?.length} songs</p>
       </div>
-      <div className="App-buttons"></div>
+      <div className="App-buttons">
+        <audio src={trackUrls[trackIndex]} controls />
+        <button onClick={goToNextTrack}>Next track</button>
+      </div>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { PlaylistTrack } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken =
+  'BQDZVDPr3VBWSCcwgcSSM15505_SPhCCz8o8gElpbk8jK5EqYhagWKNsOTvpuptUR4rLx21msbwV206TC-Ffz_ZMCd0U6jasDh5hnryysjXT3RWi42e7A2YZ-kxmbUnW09wTTDfuX_lKSi6IWajz87V_rpPqI1mvWn2jbYTHYdxGNXE-abKEKv1Tk9d8eAkSmMzsRpn6mwo540R8DjlL-Zv6odjEWYRGYt2sEFhXRRdirbaHcMT_YhV3ngVHZdAmxw1dGbTzXyLI91JBBzqWBybuiz7X65RPsMx4LsJeqCfCuX6hlKHJIRr72LuaD6X6U_Mj-Y-fkKTSNXSKpNT6SbR70RC6Bw';
+
+export const fetchTracks = async (): Promise<PlaylistTrack[]> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: PlaylistTrack[] };
+
+  return data.items;
 };
