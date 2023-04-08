87db0124d7a89ba565f4860f7ffdf7759ee56c84
toto-alt <130222034+toto-alt@users.noreply.github.com>
2023-04-08 17:34:55 +0200

    feat: chack answers

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,39 @@ import { useState } from 'react';
 import { fetchTracks } from './lib/fetchTracks';
 import { useQuery } from '@tanstack/react-query';
 import { AlbumCover } from './components';
-
-const trackUrls = [
-  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
-  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
-  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
-  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
-  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
-];
+import swal from 'sweetalert';
 
 const App = () => {
-  const [trackIndex, setTrackIndex] = useState(0);
-  const goToNextTrack = () =>
-    setTrackIndex(prevState => (prevState + 1) % trackUrls.length);
-
   const { data: tracks, isSuccess } = useQuery({
-    queryKey: [''],
+    queryKey: ['tracks'],
     queryFn: fetchTracks,
   });
   if (!isSuccess) {
     return <div>Oups, something wrong happened</div>;
   }
+
+  const [trackIndex, setTrackIndex] = useState(0);
+  const currentTrack = tracks[trackIndex];
+
+  if (currentTrack === undefined) {
+    return <div>You don't have any tracks</div>;
+  }
+
+  const goToNextTrack = () =>
+    setTrackIndex(prevState => (prevState + 1) % tracks.length);
+
+  const checkAnswer = (id: number) => {
+    const message = `It is ${
+      currentTrack.track.name
+    } by ${currentTrack.track.artists.map(({ name }) => name).join(' & ')}`;
+    if (id === trackIndex) {
+      swal('Bravo', message, 'success');
+    } else {
+      swal('Wrong', message, 'error');
+    }
+  };
   const [firstTrack, secondTrack, thirdTrack] = tracks;
+
   if (
     firstTrack === undefined ||
     secondTrack === undefined ||
@@ -41,16 +52,16 @@ const App = () => {
       </header>
       <div className="App-images">
         <p>You currently have {tracks.length} songs</p>
-        <AlbumCover track={firstTrack.track} />
+        <AlbumCover track={currentTrack.track} />
       </div>
-      <audio src={firstTrack.track.preview_url} controls autoPlay />
+      <audio src={currentTrack.track.preview_url} controls autoPlay />
       <div className="App-buttons">
         <button onClick={goToNextTrack}>Next track</button>
       </div>
       <div className="App-buttons">
-        <button>{firstTrack.track.name}</button>
-        <button>{secondTrack.track.name}</button>
-        <button>{thirdTrack.track.name}</button>
+        <button onClick={() => checkAnswer(0)}>{firstTrack.track.name}</button>
+        <button onClick={() => checkAnswer(1)}>{secondTrack.track.name}</button>
+        <button onClick={() => checkAnswer(2)}>{thirdTrack.track.name}</button>
       </div>
     </div>
   );
