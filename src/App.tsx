db72f78c86da0b99bf8d45a801f95f5d27ab79ed
toto-alt <130222034+toto-alt@users.noreply.github.com>
2023-04-08 13:13:57 +0200

    feat: update title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test</h1>
+        <h1 className="App-title">Bienvenue sur mon blind test</h1>
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
